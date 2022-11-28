e2b3571b4f588da6b8d4c6d5c68d272d1248cc19
Stephanie Contino <olivetalia@gmail.com>
2022-11-28 14:10:40 -0500

    Initial Commit

---
+++ Lab10/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab10
+//
+//  Created by Stephanie Contino on 11/28/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
